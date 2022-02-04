d92a4d26887e8065eb1d48f2a304d23204c896e3
Carrington Jones <carrington.jones47@gmail.com>
2022-02-03 22:56:45 -0600

    Worked on rendering results from Wikipedia API

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -2,23 +2,40 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const Search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api/php', {
+           const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
                     srsearch: term,
-                }
+                },
             });
-        };
+            setResults(data.query.search);
 
-        Search();
+        };
+        if (term) {
+            Search();
+        }
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                        <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -31,6 +48,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 };
