bd21c07d8690d77700f18c602dabf4409e902281
Carrington Jones <carrington.jones47@gmail.com>
2022-02-06 16:16:38 -0600

    Added time out to search render. Added second hook.

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -19,14 +19,48 @@ const Search = () => {
             setResults(data.query.search);
 
         };
-        if (term) {
+
+        if (term && !results.length) {
             Search();
+        } else {
+            const timeoutID = setTimeout(() => {
+                if (term) {
+                    Search();
+                }
+            }, 1000);
+
+            return () => {
+                clearTimeout(timeoutID)
+            }
         }
+
     }, [term]);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm,
+                },
+            });
+
+            setResults(data.query.search);
+        };
+        if (debouncedTerm) {
+            search();
+        }
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
